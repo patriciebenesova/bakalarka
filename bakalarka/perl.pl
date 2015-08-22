040eb87e24d85e352f7736fc1de18a62cb5a8431
patriciebenesova <patricie.benesova@symbiodigital.com>
2015-08-22 10:58:52 +0200

    perl

--- bakalarka/perl.pl
+++ bakalarka/perl.pl
@@ -1,8 +1,20 @@
-if (ref($r) eq "HASH") {
-        print "r is a reference to a hash.\n";
-    }
-    unless (ref($r)) {
-    	print $r
-    	
-        print "r is not a reference at all.\n";
-    }
+ #$x = "The cat caught the mouse";
+ #   @matches = ($x=~/\b\w{3}\b/g);
+ #    print for @matches;
+ 
+ #$y = "+420895895895 +421965896854 +420598632412";
+ #@pole = split /\+\d{3}/,$y;
+ #print @pole;
+
+  #$f = "The programming republic of Perl";
+   # @array=($f =~ /^(.+?e)(.*)$/);
+    #print "[0] = @array[0], [1]=@array[1], [2]=@array[2]";
+
+$g = "Bylo nás 5";
+$d = $g =~ s/5/pět/r;
+print  $d;
+   
+
+
+
+
